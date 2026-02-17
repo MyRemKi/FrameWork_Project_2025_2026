2e6596b1e5eb91de8e75c8b48d41170a2fffa011
MyRemKi <remi.kalkan01@hotmail.com>
2026-02-17 16:45:40 +0100

    first commit and modification on component, asset and (App.vue, main.js)

---
+++ clarat/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import TheWelcome from './components/Accueil.vue'
+</script>
+
+<template>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ clarat/src/components/Accueil.vue
@@ -0,0 +1,46 @@
+<script setup>
+
+</script>
+
+<template>
+  <nav>
+    <ul>
+      <li><h1>Accueil</h1></li>
+      <li><h1>Vol</h1></li>
+      <li><h1>Hotel</h1></li>
+      <li><h1>Contact Support</h1></li>
+      <li><h1>À Propos</h1></li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  border: 1px solid #e7e7e7;
+  background-color: #f3f3f3;
+}
+
+ul li {
+  display: block;
+  color: white;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+h1{
+  color: rgb(68, 193, 224);
+}
+ul li:hover {
+  background-color: rgb(68, 193, 224);
+}
+h1:hover{
+  color:rgb(193, 243, 255);
+}
+ul li h1.active {
+  background-color: #04AA6D;
+}
+</style>
